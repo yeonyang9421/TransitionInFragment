a80dc1b5a259139575d25bfcf5f025eb9a6df9fd
yeonyang9421 <chris09+>
2019-03-22 23:44:29 +0900

    fragment에서 화면전환 효과 주기 완성!

--- app/src/main/java/kr/co/woobi/imyeon/transitioninfragment/MainActivity.java
+++ app/src/main/java/kr/co/woobi/imyeon/transitioninfragment/MainActivity.java
@@ -4,7 +4,6 @@ import android.graphics.Color;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.FrameLayout;
 import android.widget.LinearLayout;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
@@ -29,25 +28,25 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         switch (v.getId()) {
             case R.id.menu1:
                 getSupportFragmentManager().beginTransaction()
-                        .setCustomAnimations(R.anim.enter_anim, R.anim.exit_anim)
+                        .setCustomAnimations(R.anim.tran1, R.anim.tran3)
                         .replace(R.id.frame, TransitionFragment
                                 .newInstance("It's your First Fragment",Color.parseColor("#3F51B5"))).commit();
                 break;
             case R.id.menu2:
                 getSupportFragmentManager().beginTransaction()
-                        .setCustomAnimations(R.anim.enter_anim2, R.anim.exit_anim)
+                        .setCustomAnimations(R.anim.tran2, R.anim.tran3)
                         .replace(R.id.frame, TransitionFragment
                                 .newInstance("It's your Second Fragment",Color.parseColor("#CDDC39"))).commit();
                 break;
             case R.id.menu3:
                 getSupportFragmentManager().beginTransaction()
-                        .setCustomAnimations(R.anim.enter_anim2, R.anim.exit_anim2)
+                        .setCustomAnimations(R.anim.tran2, R.anim.tran4)
                         .replace(R.id.frame, TransitionFragment
                                 .newInstance("It's your Third Fragment",Color.parseColor("#FF4CAF50"))).commit();
                 break;
             case R.id.menu4:
                 getSupportFragmentManager().beginTransaction()
-                        .setCustomAnimations(R.anim.enter_anim, R.anim.exit_anim2)
+                        .setCustomAnimations(R.anim.tran1, R.anim.tran4)
                         .replace(R.id.frame, TransitionFragment
                                 .newInstance("It's your Fourth Fragment",Color.parseColor("#FF5722"))).commit();
                 break;
